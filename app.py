0c9637ba466d8ab664109cbd17640cb414134c4a
vikassnwl <vikassnwl@gmail.com>
2020-09-13 18:52:32 +0530

    changes made

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, flash
 import requests, pygal
+from pygal.style import Style
 
 app = Flask(__name__)
 
@@ -7,7 +8,7 @@ app.config['SECRET_KEY'] = '\xcfNsV\x8ec\x80\x1b\xac\x07\xc0\xa0\xff\x07u\x1dow;
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    hist_data = ''
+    chart_data = ''
     if request.method == 'POST':
         lang = request.form.get('lang')
         height = request.form.get('height')
@@ -17,21 +18,35 @@ def index():
         try:
             items = data['items']
             names = []
-            stars = []
+            plot_dicts = []
             for item in items:
                 names.append(item['name'])
-                stars.append(item['stargazers_count'])
-            hist = pygal.Bar(x_label_rotation=45, truncate_label=15, show_legend=False, height=float(height)-268)
-            hist.title = 'Most-Starred '+ lang.capitalize() +' Projects on GitHub'
-            hist.x_labels = names
-            hist.add('', stars)
-            hist_data = hist.render_data_uri()
+
+                # Get the project description, if one is available.
+                description = item['description']
+                if not description:
+                    description = 'No description provided.'
+                plot_dict = {
+                    'value': item['stargazers_count'],
+                    'label': description[:80]+'...',
+                    'xlink': item['html_url']
+                }
+                plot_dicts.append(plot_dict)
+
+            custom_style = Style(
+                tooltip_font_size=10
+            )
+            chart = pygal.Bar(x_label_rotation=45, truncate_label=15, show_legend=False, height=float(height)-268, style=custom_style)
+            chart.title = 'Most-Starred '+ lang.capitalize() +' Projects on GitHub'
+            chart.x_labels = names
+            chart.add('', plot_dicts)
+            chart_data = chart.render_data_uri()
         
         except:
             flash('Invalid input')
 
 
-    return render_template('index.html', hist_data=hist_data)
+    return render_template('index.html', chart_data=chart_data)
 
 
 if __name__ == '__main__':
