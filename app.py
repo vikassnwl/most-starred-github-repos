78a30376a80b83c52f236f7515d5e1367dfce483
vikassnwl <vikassnwl@gmail.com>
2020-09-13 17:59:56 +0530

    changes made

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, flash
 import requests, pygal
 
 app = Flask(__name__)
 
+app.config['SECRET_KEY'] = '\xcfNsV\x8ec\x80\x1b\xac\x07\xc0\xa0\xff\x07u\x1dow;\nY\xe2h\x9e'
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -13,18 +14,21 @@ def index():
         url = 'https://api.github.com/search/repositories?q=language:'+lang+'&sort=stars'
         r = requests.get(url)
         data = r.json()
-        items = data['items']
-        names = []
-        stars = []
-        for item in items:
-            names.append(item['name'])
-            stars.append(item['stargazers_count'])
-        hist = pygal.Bar(x_label_rotation=45, truncate_label=15, show_legend=False, height=float(height)-268)
-        hist.title = 'Most-Starred '+ lang.capitalize() +' Projects on GitHub'
-        hist.x_labels = names
-        hist.add('', stars)
-        hist_data = hist.render_data_uri()
-        # hist.render_to_file('file.svg')
+        try:
+            items = data['items']
+            names = []
+            stars = []
+            for item in items:
+                names.append(item['name'])
+                stars.append(item['stargazers_count'])
+            hist = pygal.Bar(x_label_rotation=45, truncate_label=15, show_legend=False, height=float(height)-268)
+            hist.title = 'Most-Starred '+ lang.capitalize() +' Projects on GitHub'
+            hist.x_labels = names
+            hist.add('', stars)
+            hist_data = hist.render_data_uri()
+        
+        except:
+            flash('Invalid input')
 
 
     return render_template('index.html', hist_data=hist_data)
